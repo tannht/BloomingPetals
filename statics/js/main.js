4a9a35fafe2415d4e5d9abd1ebaf44db30a713ef
tannht <hoangtan.nht@gmail.com>
2021-06-25 02:08:46 +0700

    no message

--- statics/js/main.js
+++ statics/js/main.js
@@ -1,1 +1,12 @@
-/* MEGA MENU */
+/* ScrollTop Button */
+$(document).ready(function () {
+            /* ScrollTop Button */
+            
+            function() {
+                if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                    $(#scrollTop).style.display = "block";
+                } else {
+                    $(#scrollTop).style.display = "none";
+                }
+
+            }

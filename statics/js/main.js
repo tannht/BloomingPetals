c2a418c54de8877e1f17a698f54eb6e5c48055d0
tannht <hoangtan.nht@gmail.com>
2021-06-26 00:06:16 +0700

    no message

--- statics/js/main.js
+++ statics/js/main.js
@@ -6,5 +6,13 @@
 $(document).scroll(function () {
     if ($(this).scrollTop() > 500) {
         $('#to-top-btn').show()
-    } else $('#to-top-btn').hide()
+    } else $('#to-top-btn').hide();
+    
+});
+$(window).scroll(function() {    
+    var scrollHeight = $(window).scrollTop();
+    
+    if (scrollHeight > 0) {
+        $('.navibar').addClass("scroll");
+    } else $('.navibar').removeClass("scroll");
 });

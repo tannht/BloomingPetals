09668173b037191a3c9660d8c413679a6c178969
tannht <hoangtan.nht@gmail.com>
2021-06-25 16:48:00 +0700

    no message

--- statics/js/main.js
+++ statics/js/main.js
@@ -1,12 +1,10 @@
-/* ScrollTop Button */
-$(document).ready(function () {
-            /* ScrollTop Button */
-            
-            function() {
-                if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                    $(#scrollTop).style.display = "block";
-                } else {
-                    $(#scrollTop).style.display = "none";
-                }
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
 
-            }
+/* ScrollTop Button */
+$(document).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+        $('#to-top-btn').show()
+    } else $('#to-top-btn').hide()
+});

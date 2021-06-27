9f3118bfdbcb1b775f7487714a72846204dd7b32
tannht <hoangtan.nht@gmail.com>
2021-06-27 16:48:47 +0700

    no message

--- statics/js/main.js
+++ statics/js/main.js
@@ -7,31 +7,26 @@ $(document).scroll(function () {
     if ($(this).scrollTop() > 500) {
         $('#to-top-btn').show()
     } else $('#to-top-btn').hide();
-    
+
 });
-$(window).scroll(function() {    
+$(window).scroll(function () {
     var scrollHeight = $(window).scrollTop();
-    
+
     if (scrollHeight > 0) {
         $('.navibar').addClass("scroll");
     } else $('.navibar').removeClass("scroll");
 });
 
 //Add JSON file
-var api_ulr = '../statics/js/products-data.json'
-var productsArray = fetch(api_ulr)
-  .then(response => response.json())
-  .then(function(productsList) {
-    //GET DATA FORM PRODUCTS ARRAY
-    productsList.map(function(product){
 
-        var productName = product.name
-        var categoryName = product.catName
-        var productPrice = product.price
-        var productDesc = product.desc
-        $("#popular-product").innerHTML = '<div>${productName}</div>'
+var api_ulr = '../statics/js/products-data.json'
+fetch(api_ulr)
+    .then(response => response.json())
+    .then(function(product) {
+        var popularProducts = product.map( function(product){
+            console.log(product.name)
+            return ;
+        });
+        
     })
-    
-  })
-  .catch(err => setTimeout(console.log("Data Error"),5000))
- 
+    .catch(err => console.log("Data Error"))

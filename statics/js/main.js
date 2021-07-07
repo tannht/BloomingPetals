60b7eb821ad73a3adb133a6e760dcc2ba6ddffa5
tannht <hoangtan.nht@gmail.com>
2021-07-07 16:04:10 +0700

    update menu firefox

--- statics/js/main.js
+++ statics/js/main.js
@@ -549,3 +549,55 @@ function modal() {
     $('.total-cart').html(shoppingCart.totalCart());
     $('.total-count').html(shoppingCart.totalCount());
 }
+
+ //form
+
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const contactForm = document.getElementById('contact-form');
+const errorElement = document.getElementById('error');
+const successMsg = document.getElementById('success-msg');
+const submitBtn = document.getElementById('submit');
+  
+const validate = (e) => {
+  e.preventDefault();
+ 
+  if (name.value.length < 3) {
+    errorElement.innerHTML = 'Your name should be at least 3 characters long.';
+    return false;
+  } 
+  
+  if (!(email.value.includes('.') && (email.value.includes('@')))) {
+    errorElement.innerHTML = 'Please enter a valid email address.';
+    return false;
+  } 
+
+  if (!emailIsValid(email.value)) {
+    errorElement.innerHTML = 'Please enter a valid email address.';
+    return false;
+  }
+
+  if (message.value.length < 15) {
+    errorElement.innerHTML = 'Please write a longer message.';
+    return false;
+  }
+
+  errorElement.innerHTML = '';
+  successMsg.innerHTML = 'Thank you! I will get back to you as soon as possible.'; 
+
+  e.preventDefault();
+  setTimeout(function () {
+    successMsg.innerHTML = '';
+    document.getElementById('contact-form').reset();
+  }, 6000);
+
+  return true;
+
+}
+
+const emailIsValid = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+submitBtn.addEventListener('click', validate);

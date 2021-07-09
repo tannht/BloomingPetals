85609710bfc98186c34dcd6df44986cbbd84d406
tannht <hoangtan.nht@gmail.com>
2021-07-09 08:57:10 +0700

    edit contact link

--- statics/js/validator.js
+++ statics/js/validator.js
@@ -1,11 +1,34 @@
 function Validator(options) {
-   
+    var formElement = document.querySelector(options.form);
+    if (formElement) {
+        options.rules.forEach(function (rule) {
+            var inputElement = 
+            formElement.querySelector(rule.selector);
+            if (inputElement) {
+                inputElement.onblur = function () {
+                    if (inputElement.value) {
+                        
+                    }
+                }
+            }
+        })
+    }
 }
 
-Validator.isRequired = function() {
-    return 1
-    
+Validator.isRequired = function (selector) {
+    return {
+        selector: selector,
+        test: function () {
+            return
+        }
+    }
+
 }
-Validator.isEmail = function() {
-    return 2
+Validator.isEmail = function (selector) {
+    return {
+        selector: selector,
+        test: function () {
+            return
+        }
+    }
 }

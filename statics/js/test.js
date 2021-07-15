efa0eaba177381040fc63293877db9a868a9ee45
tannht <hoangtan.nht@gmail.com>
2021-07-15 22:04:39 +0700

    no message

--- statics/js/test.js
+++ statics/js/test.js
@@ -18,17 +18,3 @@
 
 
 
-
-fetch("https://montanaflynn-spellcheck.p.rapidapi.com/check/?text=This%20sentnce%20has%20some%20probblems.", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "7ce5b190d3msh444cb696e62c2dbp1c088bjsn026d70fdffde",
-		"x-rapidapi-host": "montanaflynn-spellcheck.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
